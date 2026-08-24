6ad1a544eae2c1316f7844f29fb823a066b3d736
WinterBit <royhemant990@gmail.com>
2026-08-24 21:13:24 +0530

    chore: setup EJS

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+const path = require("path")
 const express = require("express")
 const app = express()
 
 const PORT = 3000
-app.get("/",(req,res)=>{
-    res.send("Hello World")
+
+app.set("view engine", "ejs")
+app.set("views", path.resolve("./views"))
+
+app.get("/", (req, res) => {
+    res.render("home")
 })
 
-app.listen(PORT, ()=>{
-    console.log("Server is running at PORT : ",PORT)
+app.listen(PORT, () => {
+    console.log("Server is running at PORT : ", PORT)
 })
